8b6234bee7d707012136d55968f095180d2ad27e
Randy <stlastronomy@gmail.com>
2023-09-23 06:18:26 -0500

    Initialize

---
+++ src/test/java/StainlessWorkTableSearchValidation.java
@@ -0,0 +1,8 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StainlessWorkTableSearchValidation {
+	public static void main(String[] args) {
+		WebDriverManager.chromiumdriver().setup();
+		
+	}
+}
